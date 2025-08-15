d24c0f13004e6e14d4fae4eeb9e76f617772611d
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-08-15 22:38:33 +0530

    JS Part-1

---
+++ JS Series/Part-1/script.js
@@ -0,0 +1,56 @@
+// console.log("Hello Avinashh!");
+
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+
+// // console.log("Your Total Price is " + a + b + "Rs")
+
+// console.log(`Your total price is : ${pencilPrice + eraserPrice}`) // Template Literals
+
+
+// Conditional Statements
+
+// let firstName  = "Avinash";
+
+// if(firstName == "Avinash"){
+//     console.log(`Welcome Home ${firstName}`);
+// }
+
+// let age = 26;
+
+// if(age>= 18){
+//     console.log("Aproved! , You Can Vote");
+// }
+// else{
+//     console.log("Not Aproved! , You cannot Vote");
+// }
+
+// Else-if
+
+let totalMarks = 26;  
+
+// prompt("Enter Your Marks: ") - You can Use it as USER Input
+
+// if(totalMarks >= 90){
+//     console.log("A+ Grade");
+// }
+// else if(totalMarks >= 70){
+//     console.log("A Grade");
+// }
+// else if(totalMarks >= 50){
+//     console.log("B Grade");
+// }
+// else if(totalMarks >= 35){
+//     console.log("C Grade");
+// }
+// else if(totalMarks <= 35){
+//     console.log("You Failed Looserr!!");
+// }
+
+//Alert & Prompt
+
+// alert("Something is fishyy!") // It will PoP-Up an Error in our Page
+
+console.log("Hey Avinash Alapati, How was your Learning!")
+console.error("404 Error - Learnings Not Found")
+console.warn("Still You have Enough time , Start Now!!!")
