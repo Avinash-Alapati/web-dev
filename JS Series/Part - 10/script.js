ba72a33610fb5877b533e804d9d522acbe33e5fd
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-09-04 18:09:50 +0530

    JavaScript - 10

---
+++ JS Series/Part - 10/script.js
@@ -0,0 +1,22 @@
+// Event Bubbling
+
+let div = document.querySelector('div');
+let ul = document.querySelector('ul');
+let li = document.querySelector('li');
+
+div.addEventListener("click", function () {
+    console.log("Div Was Clicked");
+});
+
+ul.addEventListener("click", function (event) {
+    event.stopPropagation(); // it will stop Event Bubbling
+    console.log("ul Was Clicked");
+});
+
+for(li of lis){
+    li.addEventListener("click" , function (event) {
+        event.stopPropagation();
+        console.log("li was clicked");
+    });
+}
+
