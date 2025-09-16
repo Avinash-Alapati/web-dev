9c5f13fa3f08e8ae1c27dafe6eb822c5b086236c
Avinash-Alapati <avinashalapati11@gmail.com>
2025-09-16 15:28:40 +0530

    NodeJS

---
+++ Backend/NodeJS/math.js
@@ -0,0 +1,16 @@
+const sum = (a,b) => a + b;
+const multi = (a,b) => a * b;
+const pi = 3.14;
+const Avi = 26;
+
+
+// module.exports() = a Special object
+// module.exports = 26;
+
+module.exports = {
+    sum : sum,
+    mul : multi,
+    PI : pi
+}
+
+// module.exports = obj;
