17d258e8920213be42ec988bc85c1e944ede3567
Avinash-Alapati <avinashalapati11@gmail.com>
2026-02-08 22:34:36 +0530

    React Basics

--- ReactJS/basic-react-app/src/App.jsx
+++ ReactJS/basic-react-app/src/App.jsx
@@ -1,23 +1,20 @@
 import "./App.css";
-
+import Title from "./Title";
+import { Description } from "./Description";
+import ProductList from "./ProductList";
 // Our First Component
-
-function Title() {
-  return <h1>Hello World!</h1>;
-}
-
-function Description() {
-  return <h4>I am Inside The World!</h4>;
-}
 
 function App() {
   return (
-    <div>
-      <h1>This is a Component</h1>
+    <>
+      {/* <h1>This is App Component</h1>
       <p>Inside App Component we have :</p>
       <Title />
-      <Description />
-    </div>
+      <Description /> */}
+
+      <ProductList />
+      <ProductList />
+    </>
   );
 }
 
